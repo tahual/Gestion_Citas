650985af69f72dc6a15a57cd0d7e78a179adb7d9
tahual <dtahalc@miumg,edu,gt>
2025-10-20 22:56:05 -0600

    configuracion y repositio en el que se trabajara

---
+++ src/main/java/miumg/edu/gt/CITAS_MEDICAS/CitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package miumg.edu.gt.CITAS_MEDICAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miumg/edu/gt/CITAS_MEDICAS/CitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package miumg.edu.gt.CITAS_MEDICAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
